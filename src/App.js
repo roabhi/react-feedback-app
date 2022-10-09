58a36cf685ffe71f755ff1b1a8e5c9a0bebb874e
roabhi <teskostudio@gmail.com>
2022-10-09 14:07:02 +0200

    Inital commit. Explanatory code about working with loops and basic use of React syntax

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+============================================================
 
-function App() {
+  1-  Components can ONLY return 1 element
+  2-  Make your returns inside ()
+  2-  Use fragments <></> to wrap what you want to return
+
+  WE CANNOT USE class and (label) for inside Components. Use className and htmlFor instead
+
+============================================================
+*/
+
+const App = () => {
+  const title = 'page title',
+    body = 'This is a blog post',
+    comments = [
+      { id: 1, text: 'First comment' },
+      { id: 2, text: 'Second comment' },
+      { id: 3, text: 'Third comment' },
+    ],
+    commentBlock = (
+      // ? Instead of terniary operator with var ? () : () we can use var &&
+      // ? () and do not use : as else statament
+      <div className="comments">
+        <span>There are {comments.length} comments</span>
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
+    ),
+    loading = false,
+    showComments = true
+
+  if (loading) return <h1>Loading...</h1>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    /*
+    ! ======================================================================
+    ! This will not work. Only ONE element can be returned
+
+    ! return (
+      ! <h1>Hello from my app component as constant</h1>
+      ! <p>Hello from a paragraph</p>
+    !)
+
+    ! Use fragments <></> to return two or more top level elements
+    ! in JSX we DO NOT return functions as Array.map((obj) => {})
+    ! but we do Array.map((obj) => ())
+    ! ======================================================================
+    */
+    <>
+      <div className="container">
+        <h3>{title.toUpperCase()}</h3>
+        <p>{body}</p>
+        {showComments && commentBlock}
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+ * ======================================================================================
+ *  1.- Import React and createRoot from react-dom/client
+ *  2-  Create App.js and export as default
+ *  3-  Render your App component with ReactDom.render(@App, @DOMelement in index.html)
+ *  4-  Import your styles
+ * ======================================================================================
+ */
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//Default packages from react
+import React from 'react'
+//? import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
+
+//import App component
+import App from './App'
+//import defalt stylesheet
+import './index.css'
+
+/*
+?============================================
+? OLD WAY with import ReactDOM from 'react-dom'
+?============================================
+*/
+
+//? ReactDOM.render(
+//?   <React.StrictMode>
+//?     <App />
+//?   </React.StrictMode>,
+//?   document.getElementById('root')
+//? )
+
+/*
+?==========================================================
+? NEW WAY with import {createRoot} from 'react-dom/client'
+?==========================================================
+*/
+
+const container = document.getElementById('root'),
+  root = createRoot(container)
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
